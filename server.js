de2f0f3fc2dae8f2611931b4147754e4572effeb
john fury <000003coc@gmail.com>
2022-09-05 16:47:38 -0300

    modifying server for heroku

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = process.env.PORT || 8080;
+const server = http.createServer(app);
 
 app.use(express.static(path.join(__dirname, "build")));
 
