f7f8cf53785d139aa8cd432cea747dfbd4628873
john fury <000003coc@gmail.com>
2022-09-05 16:56:30 -0300

    modifying server for heroku

--- server.js
+++ server.js
@@ -3,12 +3,15 @@ const express = require("express");
 // const bodyParser = require('body-parser');
 const path = require("path");
 const app = express();
-const port = process.env.PORT || 8080;
+
 const server = http.createServer(app);
 
-app.use(express.static(path.join(__dirname, "build")));
+app.use(express.static(__dirname + '/'));
 
 // This route serves the React app
 app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
 
-app.listen(port, () => console.log(`Server listening on port ${port}`));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Our app is running on port ${ PORT }`);
+});
